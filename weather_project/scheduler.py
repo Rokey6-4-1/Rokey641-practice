28a19804da154cc57be0dd5fe06de16c31f5e695
Hyeongeun Lee <hea0919@naver.com>
2025-10-29 23:22:05 +0900

    new file:   weather_project/scheduler.py

---
+++ weather_project/scheduler.py
@@ -0,0 +1,10 @@
+import schedule, time, importlib, api_collector  # 첫 import 시 1회 실행
+
+def job():
+    importlib.reload(api_collector)  # 매시간 재실행
+
+schedule.every().hour.at(":00").do(job)  # 매 정시
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
